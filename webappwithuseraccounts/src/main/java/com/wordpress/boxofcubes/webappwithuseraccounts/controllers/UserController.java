08565edae381a853d1e4029ece393d0783d82466
emilypry <emilypry@yahoo.com>
2021-01-02 17:17:36 -0600

    adding new user by signup works on database

--- webappwithuseraccounts/src/main/java/com/wordpress/boxofcubes/webappwithuseraccounts/controllers/UserController.java
+++ webappwithuseraccounts/src/main/java/com/wordpress/boxofcubes/webappwithuseraccounts/controllers/UserController.java
@@ -1,7 +1,12 @@
 package com.wordpress.boxofcubes.webappwithuseraccounts.controllers;
 
+import com.wordpress.boxofcubes.webappwithuseraccounts.data.UserRepository;
+import com.wordpress.boxofcubes.webappwithuseraccounts.models.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,9 +21,17 @@ public class UserController{
     public String showLogin(){
         return "user/login";
     }
+    
 
     @GetMapping("signup")
     public String showSignup(){
         return "user/signup";
     }
+    @PostMapping("signup")
+    @ResponseBody
+    public String processSignup(@ModelAttribute User user){
+        userRepository.save(user);
+        return "ok, added "+user.getUsername();
+    }
+
 }
