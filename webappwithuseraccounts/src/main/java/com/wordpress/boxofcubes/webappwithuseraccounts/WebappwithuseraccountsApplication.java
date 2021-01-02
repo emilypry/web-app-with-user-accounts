0f7a461febe0a17e1d1833d04b0c31eadfafb8c2
emilypry <emilypry@yahoo.com>
2021-01-02 12:16:14 -0600

    added springboot structure

---
+++ webappwithuseraccounts/src/main/java/com/wordpress/boxofcubes/webappwithuseraccounts/WebappwithuseraccountsApplication.java
@@ -0,0 +1,13 @@
+package com.wordpress.boxofcubes.webappwithuseraccounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappwithuseraccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappwithuseraccountsApplication.class, args);
+	}
+
+}
---
+++ webappwithuseraccounts/src/test/java/com/wordpress/boxofcubes/webappwithuseraccounts/WebappwithuseraccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wordpress.boxofcubes.webappwithuseraccounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappwithuseraccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
