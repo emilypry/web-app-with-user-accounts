73691a3dce29a0482da227403d2562690d0aef62
emilypry <emilypry@yahoo.com>
2021-01-02 15:16:18 -0600

    made Data class

--- webappwithuseraccounts/src/main/java/com/wordpress/boxofcubes/webappwithuseraccounts/models/Data.java
+++ webappwithuseraccounts/src/main/java/com/wordpress/boxofcubes/webappwithuseraccounts/models/Data.java
@@ -1,5 +1,33 @@
 package com.wordpress.boxofcubes.webappwithuseraccounts.models;
 
 public class Data {
-    
+    public int id;
+    private double[] x;
+    private double[] y;
+
+    public Data(){}
+    public Data(int id, double[] x, double[] y){
+        this.id = id;
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public double[] getX(){
+        return x;
+    }
+    public void setX(double[] x){
+        this.x = x;
+    }
+    public double[] getY(){
+        return y;
+    }
+    public void setY(double[] y){
+        this.y = y;
+    }
 }
--- webappwithuseraccounts/src/main/java/com/wordpress/boxofcubes/webappwithuseraccounts/models/User.java
+++ webappwithuseraccounts/src/main/java/com/wordpress/boxofcubes/webappwithuseraccounts/models/User.java
@@ -1,15 +1,19 @@
 package com.wordpress.boxofcubes.webappwithuseraccounts.models;
 
-public class User{
+import java.util.ArrayList;
+
+public class User {
     public int id;
     private String username;
     private String password; //change later
+    private ArrayList<Data> data;
 
     public User(){}
-    public User(int id, String username, String password){
+    public User(int id, String username, String password, ArrayList<Data> data){
         this.id = id;
         this.username = username;
         this.password = password;
+        this.data = data;
     }
 
     public int getId(){
@@ -29,5 +33,11 @@ public class User{
     }
     public void setPassword(String password){
         this.password = password;
+    }
+    public ArrayList<Data> getData(){
+        return data;
+    }
+    public void setData(ArrayList<Data> data){
+        this.data = data;
     }
 }
